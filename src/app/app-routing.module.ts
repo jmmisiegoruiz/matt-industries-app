0d447183390a0a091adb3cef1889dfa3d8034038
José Matías Misiego Ruiz <matias.misiego@gmail.com>
2020-04-10 15:55:46 +0200

    projects module initial set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
